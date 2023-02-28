e518b168f3c0b571eba44e9415dad661696222c2
lesterfernandez <5958256+lesterfernandez@users.noreply.github.com>
2023-02-27 19:58:15 -0500

    feat: print length of word

--- Main.java
+++ Main.java
@@ -1,7 +1,9 @@
+import java.util.Scanner;
+
 class Main {
   public static void main(String[] args) {
     System.out.println("Enter a word:");
-    
-    //Your code will return the length of the entered String
+    String word = new Scanner(System.in).next();
+    System.out.println(word.length());
   }
 }
